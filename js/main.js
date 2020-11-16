4dc6b35c5bb5de60621a19debc4f89f152826576
VictorMiBe <59127395+VictorMiBe@users.noreply.github.com>
2020-11-16 10:30:20 -0500

    .4

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 const screen = document.getElementById('calculator-screen');
 const keys = document.getElementById('calculator-keys');
+
+const calculator = () => {
+    if (!keys) return;
+    keys.addEventListener('click', e => {
+        console.log(e);
+    });
+}
+
+calculator();
