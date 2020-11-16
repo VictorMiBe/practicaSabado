506f46145c40149228ac5226c161efeb5a2d6219
VictorMiBe <59127395+VictorMiBe@users.noreply.github.com>
2020-11-16 10:07:00 -0500

    Actualizado

--- js/main.js
+++ js/main.js
@@ -1,15 +1,2 @@
-function eval(a) {
-    if (a == 'C') {
-        screen.innerHTML = '0';
-    }
-}
-
-
-let screen = document.querySelector('.screen');
-let btn = document.querySelectorAll('.button');
-btn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        screen.innerHTML += btn.innerHTML
-        eval(btn.innerHTML);
-    });
-});
+const screen = document.getElementById('calculator-screen');
+const keys = document.getElementById('calculator-keys');
